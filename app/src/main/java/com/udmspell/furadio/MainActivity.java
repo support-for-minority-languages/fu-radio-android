4e5844181cce3efa02c4dff87bd9e754dd2d384b
ggrigory <exey13@gmail.com>
2015-08-17 15:34:21 +0400

    "Ми сярысь" экран ватсамын

--- app/src/main/java/com/udmspell/furadio/MainActivity.java
+++ app/src/main/java/com/udmspell/furadio/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
@@ -346,7 +347,17 @@ public class MainActivity extends AppCompatActivity implements TagCloudView.TagC
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
